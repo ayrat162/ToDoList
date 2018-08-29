08fd47ce3b316fe2e16543bbbd6777c2165ea73e
Airat Musin <Airat.Musin@icl-services.com>
2018-08-29 16:42:48 +0300

    removed[D[D[D[D[D[D[D[D[D[D[1;2C[C[C[C[C[C[C[C[C[C[D[D[D[3~[3~[3~[3~[3~[3~[3~ and renamed static pages[D[D[D[P[2~[a[2~

--- ToDoList/ToDoList/App_Start/RouteConfig.cs
+++ ToDoList/ToDoList/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace ToDoList
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Tasks", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
