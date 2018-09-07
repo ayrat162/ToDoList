0c391a4a5f63db31fb66845525d7b06cd09700b2
Ayrat Musin <musinayrat@gmail.com>
2018-09-08 01:43:51 +0300

    added applicationuser and clientprofile added Microsoft.AspNet.Identity.Core and Microsoft.AspNet.Identity.EntityFramework nuget packages

---
+++ ToDoList/ToDoList.DAL/Entities/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ToDoList.DAL.Entities
+{
+    class ApplicationUser : IdentityUser
+    {
+        public virtual ClientProfile ClientProfile { get; set; }
+    }
+}
---
+++ ToDoList/ToDoList.DAL/Entities/ClientProfile.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ToDoList.DAL.Entities
+{
+    class ClientProfile
+    {
+        [Key]
+        [ForeignKey("ApplicationUser")]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public virtual ApplicationUser ApplicationUser { get; set; }
+    }
+}
