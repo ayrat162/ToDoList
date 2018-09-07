4e9d8f936d7dc36e677a95ae49f2e13e852ff038
Airat Musin <Airat.Musin.GDC@ts.fujitsu.com>
2018-09-07 14:59:13 +0300

    added todotaskviewmodel

---
+++ ToDoList/ToDoList.Web/ViewModels/ToDoTaskViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using ToDoList.Web.Models;
+
+namespace ToDoList.Web.ViewModels
+{
+    public class ToDoTaskViewModel
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDateTime { get; set; }
+        public string Status { get; set; }
+        public Classification Classification { get; set; }
+        public ToDoTask ConnectedToDoTask { get; set; }
+    }
+}
