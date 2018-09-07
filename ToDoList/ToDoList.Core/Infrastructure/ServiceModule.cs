6f903bec4cdfdded2af9d64e01a13b4418a0bcec
Airat Musin <Airat.Musin.GDC@ts.fujitsu.com>
2018-09-07 18:15:18 +0300

    changed target framework to 4.6.1

---
+++ ToDoList/ToDoList.Core/Infrastructure/ServiceModule.cs
@@ -0,0 +1,19 @@
+﻿using Ninject.Modules;
+using ToDoList.DAL.Interfaces;
+using ToDoList.DAL.Repositories;
+
+namespace ToDoList.Core.Infrastructure
+{
+    public class ServiceModule : NinjectModule
+    {
+        private string connectionString;
+        public ServiceModule(string connection)
+        {
+            connectionString = connection;
+        }
+        public override void Load()
+        {
+            Bind<IUnitOfWork>().To<EFUnitOfWork>().WithConstructorArgument(connectionString);
+        }
+    }
+}
