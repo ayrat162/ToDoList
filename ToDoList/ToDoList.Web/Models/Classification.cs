7b10f5815d5d3633d71167dfa9df6e99189fe211
Airat Musin <Airat.Musin.GDC@ts.fujitsu.com>
2018-09-07 14:54:44 +0300

    created todotask and classification models, dbcontext

---
+++ ToDoList/ToDoList.Web/Models/Classification.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoList.Web.Models
+{
+    public class Classification
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Color { get; set; }
+    }
+}
---
+++ ToDoList/ToDoList.Web/Models/IdentityModels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList.Web.Models
+{
+    public class ToDoListContext: DbContext
+    {
+        public DbSet<ToDoTask> ToDoTasks { get; set; }
+        public DbSet<Classification> Classifications { get; set; }
+    }
+
+    public class ToDoListDbInitializer : DropCreateDatabaseAlways<ToDoListContext>
+    {
+        protected override void Seed(ToDoListContext db)
+        {
+            db.ToDoTasks.Add(new ToDoTask { Description = "Task 1", DueDateTime = DateTime.Now, Status = "WIP" });
+            db.ToDoTasks.Add(new ToDoTask { Description = "Task 2", DueDateTime = DateTime.Now, Status = "Suspended" });
+            db.ToDoTasks.Add(new ToDoTask { Description = "Task 3", DueDateTime = DateTime.Now, Status = "Done" });
+            db.ToDoTasks.Add(new ToDoTask { Description = "Task 4", DueDateTime = DateTime.Now, Status = "WIP" });
+            db.SaveChanges();
+        }
+    }
+}
